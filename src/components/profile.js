f89969ab078d453151d3b83b3631cb3a000076d5
mganeko <m.ganeko@gmail.com>
2020-07-20 13:15:42 +0900

    auth tutorial

---
+++ src/components/profile.js
@@ -0,0 +1,30 @@
+// import React from "react"
+
+// const Profile = () => (
+//   <>
+//     <h1>Your profile</h1>
+//     <ul>
+//       <li>Name: Your name will appear here</li>
+//       <li>E-mail: And here goes the mail</li>
+//     </ul>
+//   </>
+// )
+
+// export default Profile
+
+
+
+import React from "react"
+import { getUser } from "../services/auth"
+
+const Profile = () => (
+  <>
+    <h1>Your profile</h1>
+    <ul>
+      <li>Name: {getUser().name}</li>
+      <li>E-mail: {getUser().email}</li>
+    </ul>
+  </>
+)
+
+export default Profile
